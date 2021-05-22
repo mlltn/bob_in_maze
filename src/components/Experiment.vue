507686c56b95fe66681435bcc698de8a00bf1c24
mlltn <mlltn@pm.me>
2021-05-22 12:41:04 +0300

    tailwind settings and placeholder components

--- src/components/Experiment.vue
+++ src/components/Experiment.vue
@@ -1,24 +1,31 @@
 <template>
-<div>
   <div>
-      Experiment
+    <div>Experiment</div>
+    <div>
+      <div class="flex">
+        <div id="pictureFrame" class="flex-1 bg-black">
+          <img src="./../assets/material/Stimulus9-1.png" />
+        </div>
+        <div class="flex-1 bg-green-500">
+          <SliderMenu />
+        </div>
+      </div>
+      <div>
+        <button class="bg-red-200">next</button>
+      </div>
+    </div>
   </div>
-
-  <div>
-      <img src="./../assets/material/Stimulus9-1.png">
-  </div>
-  <div>
-      <button>next</button>
-  </div>
-</div>
 </template>
 
 <script>
-export default {
+import SliderMenu from "./SliderMenu.vue";
 
-}
+export default {
+  components: {
+    SliderMenu,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/SliderMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="slider-menu" class="bg-green-300">
+    <div>Total score 10</div>
+    <div>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value="0"
+        class="slider"
+        id="firstSlider"
+      />
+    </div>
+    <div>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value="0"
+        class="slider"
+        id="secondSlider"
+      />
+    </div>
+    <div>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        value="0"
+        class="slider"
+        id="thirdSlider"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
