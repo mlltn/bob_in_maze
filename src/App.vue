2d76bef74faa47a28a7b53a8472d1f5931b06079
mlltn <mlltn@pm.me>
2021-05-22 12:41:04 +0300

    Vue cli init and test material

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Experiment/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Experiment from './components/Experiment.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Experiment
   }
 }
 </script>
---
+++ src/components/Experiment.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div>
+      Experiment
+  </div>
+
+  <div>
+      <img src="./../assets/material/Stimulus9-1.png">
+  </div>
+  <div>
+      <button>next</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
